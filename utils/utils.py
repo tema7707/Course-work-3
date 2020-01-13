14365831f47a36ee72b5ed22d66883e71a2a0da4
danjiii <danila.krivenkov@mail.ru>
2020-01-13 20:11:53 +0300

    utils + network_utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,55 +1,25 @@
-import torch.nn as nn
-import torch
+import cv2
+import numpy as np
 
-class SpectralNorm(nn.Module):
-    def __init__(self, module, name='weight', power_iterations=1):
-        super(SpectralNorm, self).__init__()
-        self.module = module
-        self.name = name
-        self.power_iterations = power_iterations
-        if not self._made_params():
-            self._make_params()
+from PIL import Image
+from scipy.io import loadmat
+from CONSTS import MASKPATH, IMAGEPATH
 
-    def _update_u_v(self):
-        u = getattr(self.module, self.name + "_u")
-        v = getattr(self.module, self.name + "_v")
-        w = getattr(self.module, self.name + "_bar")
+def read_image(name):
+    try:
+        img = Image.open(f'{IMAGEPATH}/{name}')
+    except IOError:
+        img = None
+    return img
 
-        height = w.data.shape[0]
-        for _ in range(self.power_iterations):
-            v.data = l2normalize(torch.mv(torch.t(w.view(height, -1).data), u.data))
-            u.data = l2normalize(torch.mv(w.view(height, -1).data, v.data))
+def read_mask(name):
+    try:
+        mask_dic = loadmat(f'{MASKPATH}/{name}')
+    except FileNotFoundError:
+        mask_dic = {}
+    return mask_dic.get('groundtruth')
 
-        sigma = u.dot(w.view(height, -1).mv(v))
-        setattr(self.module, self.name, w / sigma.expand_as(w))
-
-    def _made_params(self):
-        try:
-            u = getattr(self.module, self.name + "_u")
-            v = getattr(self.module, self.name + "_v")
-            w = getattr(self.module, self.name + "_bar")
-            return True
-        except AttributeError:
-            return False
-
-    def _make_params(self):
-        w = getattr(self.module, self.name)
-
-        height = w.data.shape[0]
-        width = w.view(height, -1).data.shape[1]
-
-        u = nn.Parameter(w.data.new(height).normal_(0, 1), requires_grad=False)
-        v = nn.Parameter(w.data.new(width).normal_(0, 1), requires_grad=False)
-        u.data = l2normalize(u.data)
-        v.data = l2normalize(v.data)
-        w_bar = nn.Parameter(w.data)
-
-        del self.module._parameters[self.name]
-
-        self.module.register_parameter(self.name + "_u", u)
-        self.module.register_parameter(self.name + "_v", v)
-        self.module.register_parameter(self.name + "_bar", w_bar)
-
-    def forward(self, *args):
-        self._update_u_v()
-        return self.module.forward(*args)
+def cut_mask(image, mask, element):
+    cloth_mask = np.where(mask == element, mask, 0)
+    image_mask = cv2.bitwise_and(image, image, mask = cloth_mask)
+    return image_mask
